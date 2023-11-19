f9e1d61820d8e0d31827d755be1ac3a9863b7359
Fuocc <fedydev96@gmail.com>
2023-11-19 11:42:51 +0700

    upload source code 19/11

--- process_form.php
+++ process_form.php
@@ -1,36 +1,54 @@
 <?php
-    // Connect to database
-    $severname = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "hotel_booking";
+header("Content-Type: text/html; charset=UTF-8");
 
-    $conn = new mysqli($severname, $username, $password, $dbname);
+// Connect to database
+include_once("db_config.php");
 
+$conn = new mysqli($severname, $username, $password, $dbname);
 
-    // Check connect to database
-    if ($conn -> connect_error){
-        die ("Failed to connect to: " . $conn -> connect_error);
-    }
+// Check connection to the database
+if ($conn->connect_error) {
+    die("Failed to connect to the database: " . $conn->connect_error);
+}
 
-    // Take out data from Form
-    $name = $_POST['name'];
-    $phone = $_POST['phone'];
+// Take out data from Form
+if (isset($_POST['fullName'])) {
+    $fullName = $_POST['fullName'];
+} else {
+    $fullName = '';
+}
+
+if (isset($_POST['phoneNumber'])) {
+    $phoneNumber = $_POST['phoneNumber'];
+} else {
+    $phoneNumber = '';
+}
+
+if (isset($_POST['check_in'])) {
     $check_in = $_POST['check_in'];
+} else {
+    $check_in = '';
+}
+
+if (isset($_POST['check_out'])) {
     $check_out = $_POST['check_out'];
+} else {
+    $check_out = '';
+}
 
+// Insert data into Database
+$stmt = $conn->prepare("INSERT INTO bookings (fullName, phoneNumber, check_in, check_out) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("ssss", $fullName, $phoneNumber, $check_in, $check_out);
 
-    // Insert data into Database
-    $sql = "INSERT INTO bookings (name, phone, check_in, check_out)
-            VALUES ('$name', '$phone', '$check_in', '$check_out')";
-    
+if ($stmt->execute()) {
+    echo "Booking Successful!!";
+} else {
+    echo "Error: " . $stmt->error;
+}
 
-    if ($conn -> query($sql) === TRUE){
-        echo "Booking Successful!!";
-    }else {
-        echo "Error: ". $sql . "<br>" . $conn -> error;
-    }
+// Close the statement
+$stmt->close();
 
-    // Close the connect
-    $conn -> close();
+// Close the connection
+$conn->close();
 ?>
